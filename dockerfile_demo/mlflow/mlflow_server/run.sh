e18371c3f6b7e1381f8664d09febd94e54f2fcd7
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2023-03-22 11:44:13 +0530

    Update run.sh

--- dockerfile_demo/mlflow/mlflow_server/run.sh
+++ dockerfile_demo/mlflow/mlflow_server/run.sh
@@ -5,3 +5,5 @@ mlflow server \
     --default-artifact-root $MLFLOW_ARTIFACT_STORE_URI \
     --host $SERVER_HOST \
     --port $SERVER_PORT
+
+# mlflow server --backend-store-uri $MLFLOW_BACKEND_STORE_URI --no-serve-artifacts --host $SERVER_HOST --port $SERVER_PORT
